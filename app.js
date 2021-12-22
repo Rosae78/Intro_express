6012d496d71041131a54f8724cee21e6d5603f29
Rosa <FormacionRosaMariaAbraham@gmail.com>
2021-12-22 09:38:26 +0100

    Intro_express

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    // res.send('<h1>Inicio</>')
+    res.sendFile('./views/index.html', {root:__dirname})
 })
+app.get('/about', (req, res) => {
+    res.sendFile('./views/index.html', {root:__dirname})
+})
+
+app.use((req, res)=> {
+    res.sendFile('./views/404.html', {root:__dirname})
+})
+//Redireccionar
+app.get('/about-us', (req, res) => {
+    res.redirect('/about')
+}) 
 
 
 const PORT = process.env.PORT || 5000
